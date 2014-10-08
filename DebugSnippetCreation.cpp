9398e194d3560b7cb96d16785b60d9ae65758195
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-10-08 17:45:26 +0530

    Fixed sentence boundary issue

--- DebugSnippetCreation.cpp
+++ DebugSnippetCreation.cpp
@@ -35,7 +35,7 @@ int main() {
     cout<<text<<endl;
     size_t last_pos = 0;
     string snippet;
-    int snippet_begin = 62300;
+    int snippet_begin = 62412;
     unsigned int current_size = 0;
     size_t length = 550;
     while (snippet.size() < length) {
@@ -44,37 +44,31 @@ int main() {
 	}
 
 	size_t new_pos = text.find(". ", last_pos);
-	size_t new_posM = text.find(".^M", last_pos);
-	size_t new_posr = text.find(".\r", last_pos);
-	cout<<"new_pos"<<new_pos<<"     "<<new_posr<<new_posr<<"  "<<new_posM<<endl;
-	if(new_posr< new_pos) {
-
-		new_pos = new_posr;
-	}
-		if(new_posM < new_pos) {
-
-		new_pos = new_posM;
-	}
 	cout<<"new_pos"<<new_pos<<endl;
 	if (new_pos == string::npos) {
 	    new_pos = text.length() - 1;
+		if(new_pos-last_pos < length) {
+	    	last_pos = text.length()-length+1;
+	}    
 	}
-	/*if( new_pos - last_pos > length-3) {
-		new_pos = text.find(" ", last_pos);
-		while( last_pos-new_pos< length-3) {
-			new_pos = text.find(" ", new_pos+1);
-			cout<<new_pos<<endl;
-		}
-	}*/
+	
 	string sentence(text, last_pos, new_pos - last_pos + 1);
 	cout<<"sentence size "<<sentence.size()<<"  "<<length<<endl;
 
-	/*while(sentence.size() > length) {
-		cout<<sentence.size()<<"  "<<length<<"  "<<text.find(" ", new_pos-50)<<"  "<<new_pos<<endl;
-		new_pos = text.find(" ", new_pos - 200);
-		string sentence1(text, last_pos, new_pos - last_pos + 1);
+	if (sentence.size() > length) {
+		cout<<sentence.size()<<"  "<<length<<"  "<<text.find(" ", new_pos-50)<<"  "<<new_pos<<" ddd "<<last_pos<<endl;
+		int newlength = text.find(" ",last_pos+length-50);
+		if(newlength == -1) {
+			newlength = last_pos+length-10;
+			cout<<"new lenght"<<endl;
+		}
+		int origlen = newlength - last_pos ;
+		cout<<"newlength : "<<newlength<<"origlen  "<<origlen<<" sentence1 "<<endl;
+		new_pos = last_pos+origlen;
+		string sentence1(text, last_pos, origlen);
 		sentence = sentence1;
-	}*/
+		cout<<"sentence1 : " <<" sentence1 "<<sentence<<endl;
+	}
 
 	Xapian::Document sent_doc;
 	Xapian::TermGenerator term_gen;
@@ -83,29 +77,27 @@ int main() {
 	term_gen.index_text(sentence);
 
 	int sent_size = 0;
-	for (Xapian::TermIterator it = sent_doc.termlist_begin(); it != sent_doc.termlist_end(); ++it) {
 
+	for (Xapian::TermIterator it = sent_doc.termlist_begin(); it != sent_doc.termlist_end(); ++it) {
 	    sent_size += it.positionlist_count();
 	}
 
 	current_size += sent_size;
 	cout<<"current_size"<<current_size<<" snippet_size"<<snippet.size()<<endl;
-	if (current_size > snippet_begin)
-		if(sentence.size() + snippet.size() > length) {
-			int post = sentence.find("golf");
-			snippet += sentence.sub_str();
-		} else {
+		if (current_size > snippet_begin) {
 	    	snippet += sentence;
-	    }
-
-	last_pos = new_pos + 1;
+		}
+	    last_pos = new_pos + 1;
     }
+
     cout<<"OLD Snippet"<<endl;;
-    cout<<snippet<<endl;
+    //cout<<snippet<<endl;
     if (snippet.size() >= length) {
 	snippet.resize(length - 3);
 	snippet += "...";
     }
 	cout<<"New Snippet"<<endl;
     cout<<snippet<<endl;
+
+
 }
